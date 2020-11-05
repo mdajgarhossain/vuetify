449eeb077ccb87bbbf4c980b25c1f3145b7af2b6
mdajgarhossain <mdajgarhossain1@gmail.com>
2020-11-05 16:41:09 +0600

    Text and Colors

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,23 @@
 <template>
   <div class="home">
     <h1>Home Page</h1>
+    <p class="green white--text">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, dolore?
+      Officiis, debitis quasi ipsum illum beatae sunt pariatur quaerat totam
+      voluptatum laborum, soluta asperiores, saepe maxime. Ut quaerat
+      reprehenderit saepe!
+    </p>
+    <p class="orange lighten-1 red--text text--darken-4">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem,
+      quisquam ex? Maiores quidem, delectus assumenda quibusdam repellendus
+      nostrum animi asperiores quasi accusamus ipsum, consequatur esse
+      voluptatibus recusandae ab. Voluptate, sit.
+    </p>
+    <h1 class="display-4">Massive Display</h1>
+    <h4 class="display-1">Small Display</h4>
+    <p class="headline">This is a Headline</p>
+    <p class="subheading font-weight-bold">This is a Subheading</p>
+    <p class="caption">this is a caption</p>
   </div>
 </template>
 
